8264cfb903b16e279d4b527d371dd17e8582f46a
cheol-95 <rkdcjf0122@gmail.com>
2021-02-24 18:08:10 +0900

    refactor: antd의 Col을 포팅한 CustomCol 생성

---
+++ components/CustomCol.js
@@ -0,0 +1,16 @@
+import { Row, Col } from 'antd';
+
+const CustomCol = ({ size, style, children }) => {
+  return (
+    <Col mx={size.out.mx} sm={size.out.sm} md={size.out.md} style={style}>
+      <Row justify="center" align="left">
+        <Col xs={size.in.side.mx} sm={size.in.side.sm} md={size.in.side.md} />
+        {children instanceof Array ? children[0] : children}
+        <Col xs={size.in.side.mx} sm={size.in.side.sm} md={size.in.side.md} />
+        {children.length > 1 && children[1]}
+      </Row>
+    </Col>
+  );
+};
+
+export default CustomCol;
