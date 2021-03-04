05a7657c449832ed36d03aa45379283491dec1fe
cheol-95 <rkdcjf0122@gmail.com>
2021-03-05 01:59:03 +0900

    feat: Home page 설정

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,55 @@
 import React from 'react';
 
+import CustomCol from '../components/CustomCol';
+
+import FirstWebView from '../components/pc/First';
+import SecondWebView from '../components/pc/Second';
+import ThirdWebView from '../components/pc/Third';
+
+import FirstMobileView from '../components/mobile/First';
+import SecondMobileView from '../components/mobile/Second';
+import ThirdMobileView from '../components/mobile/Third';
+
+const size = {
+  out: {
+    mx: 24,
+    sm: 24,
+    md: 24,
+  },
+  in: {
+    side: {
+      mx: 0,
+      sm: 0,
+      md: 4,
+    },
+    center: {
+      mx: 0,
+      sm: 0,
+      md: 16,
+    },
+  },
+  banner: '1080px',
+};
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <div>
+      <CustomCol size={size} style={{ background: '#FBF7F2', height: '600px' }}>
+        <FirstWebView size={size} />
+        <FirstMobileView />
+      </CustomCol>
+
+      <CustomCol size={size} style={{ background: 'white', height: '600px' }}>
+        <SecondWebView size={size} />
+        <SecondMobileView />
+      </CustomCol>
+
+      <CustomCol size={size} style={{ background: '#F7F8FA', height: '600px' }}>
+        <ThirdWebView size={size} />
+        <ThirdMobileView />
+      </CustomCol>
+    </div>
+  );
 };
 
 export default Home;
